b63d9dba3c22b331c2113310ddc382d2007273fa
Jackson Cassimiro <jackson.c@kintsugihealth.com>
2024-05-17 19:17:00 -0300

    PROD-1363: refactoring and improvements.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# python-sdk
-Python SDK to access kintsugi voice api v2
+# Kintsugi Python SDK
+Python SDK to access Kintsugi Voice API v2.
+
+## Usage
+```
+import os
+from src.kintsugi.api import Api
+
+
+if __name__ == '__main__':
+    # Configuration Parameters
+    x_api_key = os.environ['X_API_KEY']
+    user_id = os.environ['USER_ID']
+    url = os.environ['URL']
+
+    # Parameters
+    audio_file_1 = open('/Users/jackson/Downloads/test_audio.wav', 'rb')
+    audio_file_2 = open('/Users/jackson/Downloads/test_audio.wav', 'rb')
+    allowed_sample_rate = 44100
+    metadata = {
+        'age': 39,
+        'gender': 'male'
+    }
+
+    # API instantiation
+    api = Api(x_api_key=x_api_key, url=url)
+
+    # Prediction using all arguments
+    api.prediction().predict(
+        user_id,
+        audio_file_1,
+        metadata=metadata,
+        allowed_sample_rate=allowed_sample_rate,
+    )
+
+    # Prediction using fewer arguments
+    session_id = api.prediction().predict(user_id, audio_file_2)
+
+    # Get prediction by session
+    prediction_response = api.prediction().get_prediction_by_session(session_id)
+
+    # Get predictions by user
+    predictions_user = api.prediction().get_prediction_by_user(user_id)
+
+    # Depression feedback
+    api.feedback().depression(session_id, 'false')
+
+    # PHQ2 feedback
+    api.feedback().phq_2(session_id, [1, 2])
+
+    # PHQ9 feedback
+    api.feedback().phq_2(session_id, [1, 2, 1, 2, 1, 2, 3, 1, 2])
+```
--- src/kintsugi/api.py
+++ src/kintsugi/api.py
@@ -12,12 +12,9 @@ class ResponseException(Exception):
 
 
 class Api:
-    def __init__(self, user_id: str, is_initiated: bool, x_api_key: str, url: str, metadata: dict=None):
-        self.user_id: str = user_id
-        self.is_initiated: bool = is_initiated
+    def __init__(self, x_api_key: str, url: str):
         self.x_api_key = x_api_key
         self.url: str = url
-        self.metadata: dict = metadata if metadata is not None else {}
 
     def get_common_headers(self):
         return {
@@ -25,12 +22,15 @@ class Api:
             'X-API-Key': self.x_api_key,
         }
 
-    def new_session_id(self) -> str:
+    def new_session_id(self, user_id: str, metadata: dict = None) -> str:
         data = {
-            'is_initiated': self.is_initiated,
-            'metadata': self.metadata,
-            'user_id': self.user_id,
+            'is_initiated': True,
+            'user_id': user_id,
         }
+
+        if metadata:
+            data['metadata'] = metadata
+
         response = requests.post(
             f'{self.url}/initiate',
             headers=self.get_common_headers(),
@@ -41,10 +41,10 @@ class Api:
 
         return response.json()['session_id']
 
-    def prediction(self):
+    def prediction(self) -> 'PredictionHandler':
         return PredictionHandler(self)
 
-    def feedback(self):
+    def feedback(self) -> 'FeedbackHandler':
         return FeedbackHandler(self)
 
 
@@ -53,14 +53,19 @@ class PredictionHandler:
     def __init__(self, api: Api):
         self.api = api
 
-    def predict(self, audio_file) -> str:
+    def predict(self, user_id: str, audio_file, metadata:dict = None, allowed_sample_rate: int = None) -> str:
+        data = {
+            'session_id': self.api.new_session_id(user_id, metadata),
+        }
+
+        if allowed_sample_rate:
+            data['allowed_sample_rate'] = allowed_sample_rate
+
         response = requests.post(
             f'{self.api.url}/prediction/',
             headers=self.api.get_common_headers(),
             files={'file': audio_file},
-            data={
-                'session_id': self.api.new_session_id(),
-            },
+            data=data,
         )
 
         if response.status_code != 202:
