17fc00186065ed9043ec6911a42e6f42539e5110
Jackson Cassimiro <jackson.c@kintsugihealth.com>
2024-05-20 18:19:02 -0300

    Refactoring feedback parsing.

--- src/kintsugi/parsers.py
+++ src/kintsugi/parsers.py
@@ -1,6 +1,24 @@
 from src.kintsugi.model import Prediction, FeedbackScore
 
 
+class FeedbackScoreParser:
+     def parse(self, data: dict) -> FeedbackScore:
+         feedback = FeedbackScore()
+
+         category_label = [
+             (key, value) for key, value in data.items()
+             if key not in ['phq_2', 'phq_9']
+         ]
+         if category_label:
+             feedback.category = category_label[0][0]
+             feedback.label = category_label[0][1]
+
+         feedback.phq_2 = data.get('phq_2', [])
+         feedback.phq_9 = data.get('phq_9', [])
+
+         return feedback
+
+
 class PredictionParser:
     def parse(self, data: dict) -> Prediction:
         output = Prediction()
@@ -21,19 +39,6 @@ class PredictionParser:
             output.predicted_score = predicted_data
 
         if 'actual_score' in data:
-            feedback_data = data['actual_score']
-            feedback = FeedbackScore()
-
-            category_label = [
-                (key, value) for key, value in feedback_data.items()
-                if key not in ['phq_2', 'phq_9']
-            ]
-            if category_label:
-                feedback.category = category_label[0][0]
-                feedback.label = category_label[0][1]
-
-            feedback.phq_2 = feedback_data.get('phq_2', [])
-            feedback.phq_9 = feedback_data.get('phq_9', [])
-            output.feedback_score = feedback
+            output.feedback_score = FeedbackScoreParser().parse(data['actual_score'])
 
         return output
